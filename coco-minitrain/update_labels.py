944f7973e5eddfbbaf9135fd1bef21ea34b399dc
BBC <bryan.bo.cao@outlook.com>
2022-06-20 20:23:52 -0400

    update_labels.py

--- coco-minitrain/update_labels.py
+++ coco-minitrain/update_labels.py
@@ -68,3 +68,50 @@ if __name__ == '__main__':
 
         assert len(C.img_id_ls) == 8000
         print(len(C.img_id_ls)) # 8000
+
+    print('---------------------------')
+
+    txt_path = C.dataset_root_path + '/train2017.txt'
+    print(txt_path)
+    txt_path_ORI = C.dataset_root_path + '/train2017_ORI.txt'
+    if not os.path.exists(txt_path_ORI):
+        cmd = 'scp {} {}'.format(txt_path, txt_path_ORI); os.system(cmd); print(cmd)
+        cmd = 'rm {}'.format(txt_path); os.system(cmd); print(cmd)
+        cmd = 'touch {}'.format(txt_path); os.system(cmd); print(cmd)
+
+    label_root_path = C.dataset_root_path + '/labels'
+    print(label_root_path)
+    label_root_path_ORI = C.dataset_root_path + '/labels_ORI'
+    label_train_path = label_root_path + '/train2017'
+    if not os.path.exists(label_root_path_ORI):
+        cmd = 'scp -r {} {}'.format(label_root_path, label_root_path_ORI); os.system(cmd); print(cmd)
+        cmd = 'rm -r {}'.format(label_train_path); os.system(cmd); print(cmd)
+        os.makedirs(label_train_path)
+
+    label_val_path = label_root_path + '/val2017'
+    if os.path.exists(label_val_path):
+        label_val_path_ORI = label_root_path_ORI + '/val2017'
+        cmd = 'scp -r {} {}'.format(label_val_path_ORI, label_val_path); os.system(cmd); print(cmd)
+
+    print('---------------------------')
+
+    txt_file = open(txt_path, 'w')
+    for img_id in C.img_id_ls:
+        # ----------------------
+        #  Update train2017.txt
+        # ----------------------
+        line_to_write = './images/train2017/{}.jpg\n'.format(img_id)
+        txt_file.write(line_to_write)
+        print(line_to_write, 'written!')
+
+        # ---------------------------
+        #  Update labels/train2017/*
+        # ---------------------------
+        label_path = label_root_path + '/train2017/' + img_id + '.txt'
+        label_path_ORI = label_root_path_ORI + '/train2017/' + img_id + '.txt'
+        cmd = 'scp {} {}'.format(label_path_ORI, label_path); os.system(cmd); print(print)
+        print(cmd)
+
+    # Clean intermediate folders
+    cmd = 'rm -r {}'.format(label_root_path_ORI); os.system(cmd); print(cmd)
+    cmd = 'rm -r {}'.format(txt_path_ORI); os.system(cmd); print(cmd)
